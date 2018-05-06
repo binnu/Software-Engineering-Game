e61099fafae3979e2c3c57687dca352f07fb6fdd
Unknown <bindumadhavi.pesaladinne@student.fairfield.edu>
2018-05-06 13:54:19 -0400

    New changes

---
+++ SoftwareEngineeringGame/Assets/Scripts/LoginRegisterNavigation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginRegisterNavigation : MonoBehaviour {
+
+    public void LoginButton()
+    {
+
+        Application.LoadLevel("Login");
+    }
+
+    public void RegisterButton()
+    {
+
+        Application.LoadLevel("Register");
+    }
+}
